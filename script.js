21f71b49b0f6d546c3b7c3eb7fed441663092ced
rhemanth399 <120007414+rhemanth399@users.noreply.github.com>
2023-01-05 19:15:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+const textbox = document.getElementById('evaluatedText');
+const letterCountElement = document.getElementById('letterCount');
+
+textbox.addEventListener('keyup', function() {
+  const letterCount = textbox.value.length;
+  letterCountElement.innerHTML = letterCount;
+});
